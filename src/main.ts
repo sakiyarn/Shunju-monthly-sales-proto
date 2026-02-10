13146429614c397ffd7c8121935db8abfc719e1c
sakium <sakiyama.303911@gmail.com>
2026-02-10 18:53:44 +0900

    feat: 案件別月次売上サマリーのプロトタイプ

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import { AllCommunityModule, ModuleRegistry, provideGlobalGridOptions } from 'ag-grid-community'
+import App from './App.vue'
+import './style.css'
+
+ModuleRegistry.registerModules([AllCommunityModule])
+provideGlobalGridOptions({ theme: 'legacy' })
+
+createApp(App).mount('#app')
